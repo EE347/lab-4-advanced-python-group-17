eea389cdfed01321c6b055a21d1185299186ee1c
Tadhg Donoghue <t.donoghue2@universityofgalway.ie>
2024-10-09 16:09:41 +0100

    Task7

--- task7.py
+++ task7.py
@@ -1,5 +1,12 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 x = np.linspace(0, 10, 101)
-print(x)
+sine_wave = np.sin(x)
+cosine_wave = np.cos(x)
+
+
+
+np.save('task7_sin.npy', sine_wave)
+np.save('task7_cos.npy', cosine_wave)
+
