8d0e5cef08a90bb32dc95a8d01356557573fc1be
Tadhg Donoghue <t.donoghue2@universityofgalway.ie>
2024-10-09 16:10:53 +0100

    Task8

--- task8.py
+++ task8.py
@@ -1,2 +1,24 @@
 import numpy as np
+
 import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 101)
+sine_wave = np.sin(x)
+cosine_wave = np.cos(x)
+
+
+
+np.save('task7_sin.npy', sine_wave)
+np.save('task7_cos.npy', cosine_wave)
+
+
+
+plt.plot(x, sine_wave, label='Sine ')
+plt.plot(x, cosine_wave, label='Cos ')
+
+plt.xlabel('x ')
+plt.ylabel('y ')
+plt.title('Waves')
+plt.legend()
+
+plt.show()
